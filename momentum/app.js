d72688908d89bad82b893af84d3340fd9753dd62
theines <churn8282@naver.com>
2021-09-30 17:22:33 +0900

    classList is awesome

--- momentum/app.js
+++ momentum/app.js
@@ -1,12 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-//색은 css에서 하고 javascript에서는 className을 줬다뺐으면서 효과 주기
+
 function handleTitleClick(){
     const clickedClass = "clicked";
-    if (h1.className === "clickedClass"){
-        h1.className = "";//클릭하면 다시 클래스네임을 공백으로 만들고 
+    if (h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
     } else{
-        h1.className = "clickedClass";//여기에서 className을 지정해주고
+        h1.classList.add(clickedClass);
     }
 }
 
