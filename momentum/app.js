f79587983e45c8136a8b0a42717488087f7925df
theines <churn8282@naver.com>
2021-09-30 15:38:54 +0900

    className을 줬다 뺏었다 하면서 효과 줘보기

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
+//색은 css에서 하고 javascript에서는 className을 줬다뺐으면서 효과 주기
+function handleTitleClick(){
+    if (h1.className === "active"){
+        h1.className = "";//클릭하면 다시 클래스네임을 공백으로 만들고 
+    } else{
+        h1.className = "active";//여기에서 className을 지정해주고
+    }
 }
 
-window.addEventListener("resize", handleWindowResize);
-//hey
-//this is the test
+h1.addEventListener("click", handleTitleClick);
