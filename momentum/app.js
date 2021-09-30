841f47b304a19a19ff4f285b389d33e79735286c
theines <churn8282@naver.com>
2021-09-30 15:46:26 +0900

    reason 4 having className as Variable

--- momentum/app.js
+++ momentum/app.js
@@ -2,10 +2,11 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 //색은 css에서 하고 javascript에서는 className을 줬다뺐으면서 효과 주기
 function handleTitleClick(){
-    if (h1.className === "active"){
+    const clickedClass = "clicked";
+    if (h1.className === "clickedClass"){
         h1.className = "";//클릭하면 다시 클래스네임을 공백으로 만들고 
     } else{
-        h1.className = "active";//여기에서 className을 지정해주고
+        h1.className = "clickedClass";//여기에서 className을 지정해주고
     }
 }
 
