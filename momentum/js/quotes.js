92725e99d057427280de72c9035d798e2a75c0b5
theines <churn8282@naver.com>
2021-10-01 19:47:25 +0900

    javascript를 통해서 html에 태그 넣기

---
+++ momentum/js/quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+    {
+        quote:"Try again. Fail again. Fail better.",
+        author:"Samuel Beckett"
+    },
+    {
+        quote:"When you look long into an abyss, The abyss looks into you.",
+        author:"Friedrich Nietzsche"
+    },
+    {
+        quote:"All that glitters is not gold.",
+        author:"W. Shakespeare"
+    },
+    {
+        quote:"The irrationality of a thing is not an argument against it's existence, Rather, a condition of it.",
+        author:"Friedrich Nietzsche"
+    },
+    {
+        quote:"Whatever is begun in anger, ends in shame.",
+        author:"B. Franklin"
+    },
+    {
+        quote:"The man being is a blind man who dreams that he can see.",
+        author:"Hebbel"
+    },
+    {
+        quote:"His own character is the arbiter of every one’s fortune.",
+        author:"P. Syrus"
+    },
+    {
+        quote:"Act as men of thought; think as men of action.",
+        author:"H. Bergson"
+    },
+    {
+        quote:"Slow but sure wins the race.",
+        author:"S. Smiles"
+    },
+    {
+        quote:"Nothing great was ever achieved without enthusiasm.",
+        author:"R. W. Emerson"
+    }
+
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
