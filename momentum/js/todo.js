2a352fe34c45bca41b7e40d97a69c918ac658a7d
theines <churn8282@naver.com>
2021-10-01 20:27:26 +0900

    부모요소를 이용해서 삭제 기능 추가

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    //console.log(event.target.parentElement); //이렇게 하면 어떤 버튼이 선택되었는지 알 수 있다.(부모요소 li 나옴)
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);//li 밑에 span 이라는 자식 두기
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);//li 밑에 span 이라는 자식 두기
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
