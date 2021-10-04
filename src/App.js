be4859aa9e8717c8c8f3e07eb28a53d043b8723d
theines <90564560+theines@users.noreply.github.com>
2021-10-04 18:21:32 +0900

    delete files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+// 처음엔 빈 HTML 파일이 로드되고 react가 자바스크립트를 만들어서 html로 밀어넣어준다.=>이 개념이 virtual DOM
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;
