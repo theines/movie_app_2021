a2391b4c02c9f5132e4ad47a73dbc38d55b45a90
theines <churn8282@naver.com>
2021-10-04 13:34:52 +0900

    I got the weather!!

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,10 +1,23 @@
+const API_KEY = "192a4ada39fb601a2778a4f7127ba06c";
 
 function onGeoOk(position){//geolocation object를 유일한 매개변수로 받는 콜백 함수.
     //javascript가 user의 위치를 전달해 줄 것이다. 확인해보자.
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;    
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    //fetch(url)하면 javascript가 알아서 url을 네트워크 요청을 해 줄 것이다.
+    //fetch는 해당 url로 어떠한 서버에 요청을 하게 되는데 응답 시간이 5분이 걸릴 수도 있고 그래서 then을 쓴다. 
+    fetch(url)
+        .then(response => response.json()
+        .then(data =>{
+            const weather = document.querySelector("#weather span:first-child");    
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `\n오늘의 온도 : ${data.main.temp}\n
+                                체감 온도: ${data.main.feels_like}\n
+                                최고 기온: ${data.main.temp_max}\n
+                                최저 기온: ${data.main.temp_min}\n`;
+    }));   
 }
 
 function onGEoError(){
@@ -15,3 +28,4 @@ function onGEoError(){
 //첫번째 매개변수는 모든게 잘 됐을 때 실행 될 함수.
 //나머지 하나는 에러가 발생했을 때 실행 될 함수.
 navigator.geolocation.getCurrentPosition(onGeoOk, onGEoError);
+
