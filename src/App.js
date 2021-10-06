c794c71a76a43c6bf8e5885ebdb104bf48dfb897
theines <90564560+theines@users.noreply.github.com>
2021-10-07 00:01:35 +0900

    #6 0 Getting ready for the Router(moved codes from App.js to Home.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,7 @@
-// #5 1 Are we done?
-// last thing to know is that we don't need the class component anymore
-// to have "state".
-// it's because "react hook"(new idea)
+// #6 0 Getting ready for the Router
 
-import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false })//({movies:movies})원래 이런형식이고 one from state, the other from axios
-
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state; //this.state.isLoading 매번 하기 귀찮아서
-    return (
-      /* 여기 원래 <section class="container"> 이랬는데 그냥 class하면 
-        javascript안에서는 component class에 의해 리액트가 혼란스러워져서 className으로 바꿈
-      */
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
-
+function App(){
+  return <span>sth</span>;
 }
 
 export default App;
-
