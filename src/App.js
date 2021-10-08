9cc3401ec9c353b870b6d0570662af10d7ed9f45
theines <90564560+theines@users.noreply.github.com>
2021-10-08 13:09:26 +0900

    #6-2 Building the Navigation(Link)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-// #6 1 Building the Router
-import { HashRouter, Route} from "react-router-dom";
+// #6 2 Building the Navigation
+
+// Link는 Router 안에 있어야 쓸 수 있다.
+
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 import About from "./routes/About";
 
-// <Route>에는 중요한 props 두 개가 들어간다.
-// 1. path : 랜더링할 스크린
-// 2. component : 뭘 보여줄지 정해주는 
-// <Route path="/about" component={About} /> 이렇게 해도 되는데 /이걸로 시작하는 다른 뭔가가
-// 또 있으면 겹쳐서 렌더링하기 때문에 exact={true} 해줌
-
-function App(){
-  return <HashRouter>
-    <Route path="/" exact={true} extcomponent={Home}/>
-    <Route path="/about" component={About} />
-  </HashRouter>;
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+    
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navigation(){
+    return (
+        <div>
+            {/* 그냥 a태그로 하게 되면 새로고침되면서 리액트가 죽는다 그래서 링크사용 */}
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </div>
+    );
+}
+
+export default Navigation;
